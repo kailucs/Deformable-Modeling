1742fc0ba4473349a131a5af2360e3f64bc74c8d
DeerKK <lu-k16@mails.tsinghua.edu.cn>
2019-07-18 18:42:00 -0400

    Update data_collection_process_pcd.py

--- utils/data_collection_process_pcd.py
+++ utils/data_collection_process_pcd.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
+#import logging
+#logging.basicConfig(level=logging.INFO)
 import time
 import numpy as np
 import cv2
@@ -13,18 +13,20 @@ import random
 import matplotlib.pyplot as plt
 from scipy import signal
 from open3d import *
+import os
 
 ### Constants and Measurements
 #tableHeight = 0.85
 #tableHeight = 0.859
 #tableHeight = 0.855
+'''
 tableHeight = 0.865
 probeLength = 0.1
 forceLimit = 5
 dt=0.008
 moveStep=0.002*dt   #2mm /s
-
-
+# note: dt and movestep no use?
+'''
 
 def display_inlier_outlier(cloud, ind):
 	inlier = select_down_sample(cloud, ind)
@@ -34,68 +36,82 @@ def display_inlier_outlier(cloud, ind):
 	inlier.paint_uniform_color([0.8,0.8,0.8])
 	draw_geometries([inlier,outlier])
 
-def run_collection_process_PCD():
-	II = 4
-	####### Process Pcd data ###############
-	#originalPcd = read_point_cloud("calibration_data/objectScan_phantom.xyzrgb",format='xyzrgb')
-	originalPcd = read_point_cloud("experiment_data/objectScan_"+str(II)+".xyzrgb",format='xyzrgb')
-	draw_geometries([originalPcd])
+def run_collection_process_PCD(config):
+	"""
+	this is the main function.
+	"""
 
-	### remove outlier
-	originalPcd,ind=statistical_outlier_removal(originalPcd,nb_neighbors=20,std_ratio=0.1)
-	#originalPcd,ind=radius_outlier_removal(originalPcd,nb_points=5,radius=0.05)
-	#display_inlier_outlier(originalPcd, ind)
+	tableHeight = config.tableHeight
+	probeLength = config.probeLength
+	forceLimit = config.forceLimit
+	dt = config.dt
+	moveStep = 0.002*dt
 
-	draw_geometries([originalPcd])
-	#show attributes of originalPcd
-	#print dir(originalPcd)
-	#estimate the normals
+	if not os.path.exists(config.exp_path+'exp_'+str(config.exp_number)+"/processed/"):
+		os.mkdir(config.exp_path+'exp_'+str(config.exp_number)+"/processed/")
 
-	estimate_normals(originalPcd, search_param = KDTreeSearchParamHybrid(radius = 0.1, max_nn = 30)) # normals can actually point downwards......
-	#draw_geometries([originalPcd])
+	for i in range(0,config.num_pcd):
+		II = i
+		####### Process Pcd data ###############
+		#originalPcd = read_point_cloud("calibration_data/objectScan_phantom.xyzrgb",format='xyzrgb')
+		originalPcd = read_point_cloud(config.exp_path+'exp_'+str(config.exp_number)+"/objectScan_"+str(II)+".xyzrgb",format='xyzrgb')
+		draw_geometries([originalPcd])
+
+		### remove outlier
+		originalPcd,ind=statistical_outlier_removal(originalPcd,nb_neighbors=20,std_ratio=0.1)
+		#originalPcd,ind=radius_outlier_removal(originalPcd,nb_points=5,radius=0.05)
+		#display_inlier_outlier(originalPcd, ind)
+
+		draw_geometries([originalPcd])
+		#show attributes of originalPcd
+		#print dir(originalPcd)
+		#estimate the normals
+
+		estimate_normals(originalPcd, search_param = KDTreeSearchParamHybrid(radius = 0.1, max_nn = 30)) # normals can actually point downwards......
+		#draw_geometries([originalPcd])
+
+		### remove table and points that are too "flat"
+		pcdSegmented = PointCloud()
+		xyz=[]
+		rgb=[]
+		ptNormals=[]
+
+		for (indPt,ptColor,ptNormal) in zip(originalPcd.points,originalPcd.colors,originalPcd.normals):
+			## normalize the normal
+			unitNormal=vectorops.unit(ptNormal)
+			if (indPt[2] > tableHeight):# and (abs(unitNormal[2])>0.866): #60 degrees
+				xyz.append(indPt)
+				rgb.append(ptColor)
+				if unitNormal[2] > 0:
+					ptNormals.append(ptNormal)
+				else:
+					ptNormals.append(vectorops.mul(ptNormal,-1.0))
+
+		pcdSegmented.points = Vector3dVector(np.asarray(xyz,dtype=np.float32))
+		pcdSegmented.colors = Vector3dVector(np.asarray(rgb,dtype=np.float32))
+		pcdSegmented.normals = Vector3dVector(np.asarray(ptNormals,dtype=np.float32))
+
+		draw_geometries([pcdSegmented])
+
+		write_point_cloud(config.exp_path+'exp_'+str(config.exp_number)+"/processed/objectScan_"+str(II)+".pcd",pcdSegmented)
+
+		## save as pcd 
+
+		## Downsample and save
+		'''
+		### Down sample the points for data collection
+		pcdSegmentedDown= voxel_down_sample(pcdSegmented, voxel_size = 0.005)
+		#NofPoints=countPts(pcdSegmentedDown)
+		#print countPts(pcdSegmentedDown)
+		draw_geometries([pcdSegmentedDown])
 
 
-	### remove table and points that are too "flat"
-	pcdSegmented = PointCloud()
-	xyz=[]
-	rgb=[]
-	ptNormals=[]
-
-	for (indPt,ptColor,ptNormal) in zip(originalPcd.points,originalPcd.colors,originalPcd.normals):
-		## normalize the normal
-		unitNormal=vectorops.unit(ptNormal)
-		if (indPt[2] > tableHeight):# and (abs(unitNormal[2])>0.866): #60 degrees
-			xyz.append(indPt)
-			rgb.append(ptColor)
-			if unitNormal[2] > 0:
-				ptNormals.append(ptNormal)
-			else:
-				ptNormals.append(vectorops.mul(ptNormal,-1.0))
-
-	pcdSegmented.points = Vector3dVector(np.asarray(xyz,dtype=np.float32))
-	pcdSegmented.colors = Vector3dVector(np.asarray(rgb,dtype=np.float32))
-	pcdSegmented.normals = Vector3dVector(np.asarray(ptNormals,dtype=np.float32))
-
-	draw_geometries([pcdSegmented])
-	write_point_cloud("experiment_data/processed/objectScan_"+str(II)+".pcd",pcdSegmented)
-
-	## save as pcd 
-
-	## Downsample and save
-	'''
-	### Down sample the points for data collection
-	pcdSegmentedDown= voxel_down_sample(pcdSegmented, voxel_size = 0.005)
-	#NofPoints=countPts(pcdSegmentedDown)
-	#print countPts(pcdSegmentedDown)
-	draw_geometries([pcdSegmentedDown])
-
-
-	################### Need to save this point cloud ##################
-	#each row should have position color normal
-	pcdData=open('calibration_data/probePcd.txt','w')
-	for (indPt,ptColor,ptNormal) in zip(pcdSegmentedDown.points,pcdSegmentedDown.colors,pcdSegmentedDown.normals):
-		pcdData.write(str(indPt[0])+' '+str(indPt[1])+' '+str(indPt[2])+' ')
-		pcdData.write(str(ptColor[0])+' '+str(ptColor[1])+' '+str(ptColor[2])+' ')
-		pcdData.write(str(ptNormal[0])+' '+str(ptNormal[1])+' '+str(ptNormal[2])+'\n')
-	pcdData.close()
-	'''
+		################### Need to save this point cloud ##################
+		#each row should have position color normal
+		pcdData=open('calibration_data/probePcd.txt','w')
+		for (indPt,ptColor,ptNormal) in zip(pcdSegmentedDown.points,pcdSegmentedDown.colors,pcdSegmentedDown.normals):
+			pcdData.write(str(indPt[0])+' '+str(indPt[1])+' '+str(indPt[2])+' ')
+			pcdData.write(str(ptColor[0])+' '+str(ptColor[1])+' '+str(ptColor[2])+' ')
+			pcdData.write(str(ptNormal[0])+' '+str(ptNormal[1])+' '+str(ptNormal[2])+'\n')
+		pcdData.close()
+		'''
