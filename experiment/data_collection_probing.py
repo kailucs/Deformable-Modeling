ac0a1b91ee7513cbaaf90db8e864065a0409ae6e
DeerKK <lu-k16@mails.tsinghua.edu.cn>
2019-07-19 10:14:58 -0400

    add normals

--- experiment/data_collection_probing.py
+++ experiment/data_collection_probing.py
@@ -1,5 +1,3 @@
-#import logging
-#logging.basicConfig(level=logging.INFO)
 import time
 import numpy as np
 import cv2
@@ -14,27 +12,6 @@ import random
 from robot_api.RobotController import UR5WithGripperController
 import matplotlib.pyplot as plt
 from scipy import signal
-
-#emulator
-#from RobotControllerEmulator import kill_controller_threads,UR5WithGripperController
-
-'''
-#CONTROLLER = 'simulation'
-CONTROLLER = 'physical'
-#CONTROLLER = 'debugging'
-
-### Constants and Measurements
-tableHeight = 0.87
-probeLength = 0.09
-forceLimit = 2.0
-dt=0.004  #250Hz
-moveStep=0.002*dt   #2mm /s
-shortServoTime=1.5
-longServoTime=3
-IKErrorTolerence=4
-maxDev=1.2
-EEZLimit=0.956
-'''
 
 def controller_2_klampt(robot,controllerQ):
     qOrig=robot.getConfig()
@@ -65,16 +42,11 @@ def constantVServo(controller,servoTime,target,dt):
     return 0
 
 def run_poking(config):
+	"""
+	this is the main function of poking object.
+	"""
 	##################################### Start here ################################
-	## Initialize things 
-	world = WorldModel()
-	res = world.readFile(config.robot_model_path)
-	robot = world.robot(0)
-	ee_link=config.ee_link_number
-	link=robot.link(ee_link)
-	CONTROLLER = config.mode
-
-	### Constants and Measurements
+	## Constants and Measurements
 	tableHeight = config.tableHeight
 	probeLength = config.probeLength
 	forceLimit = config.forceLimit
@@ -82,10 +54,18 @@ def run_poking(config):
 	moveStep=0.002*dt   #2mm /s
 
 	shortServoTime=config.shortServoTime
-	longServoTime=config.longServoTime+2 #TODO: set in config?
+	longServoTime=config.longServoTime + 2 #TODO: set in config?
 	IKErrorTolerence=config.IKErrorTolerence
 	maxDev=config.maxDev
 	EEZLimit=config.EEZLimit
+
+	## Initialize things 
+	world = WorldModel()
+	res = world.readFile(config.robot_model_path)
+	robot = world.robot(0)
+	ee_link=config.ee_link_number #UR5 model is 7.
+	link=robot.link(ee_link)
+	CONTROLLER = config.mode
 
 	print '---------------------model loaded -----------------------------' 
 
@@ -103,19 +83,14 @@ def run_poking(config):
 
 	print '---------------------pcd loaded -----------------------------' 
 
-	# TODO: no use
-	if CONTROLLER == 'simulation':
-		controllerWorld = world.copy()
-
 	# control interface
-	elif CONTROLLER == 'physical':
+	if CONTROLLER == 'physical':
 		robotControlApi = UR5WithGripperController(host=config.robot_host,gripper=False)
 		robotControlApi.start()
 		time.sleep(2)
 	print '---------------------robot started -----------------------------'
 
 	## Record some home configuration
-	homeConfig2=config.home_config2#controller format
 	intermediateConfig = config.intermediateConfig
 	homeConfig2 = intermediateConfig
 
@@ -123,17 +98,15 @@ def run_poking(config):
 		constantVServo(robotControlApi,4,homeConfig2,dt)#controller format
 
 	robot.setConfig(controller_2_klampt(robot,homeConfig2))
-	EETransform=link.getTransform()
+	EETransform=link.getTransform()# TODO: no use?
 	print '---------------------at home configuration -----------------------------'
-
-
 
 	if CONTROLLER == 'debugging':
 		vis.add("world",world)
 		differences=[]
 		print('[*]Debug: Poking process start')
 		#for i in range(len(points)):
-		# TODO:
+		# TODO: this is not a little stupid...
 		point_list = input('There are %d poking point, input a list:'%len(points))
 		if point_list == len(points):
 			point_list == range(len(points))
@@ -143,12 +116,11 @@ def run_poking(config):
 			print points[i],normals[i]
 			robotCurrentConfig=homeConfig2
 			goalPosition=deepcopy(points[i])
-			approachVector=vectorops.unit(vectorops.mul(normals[i],-1.0))
-			#print 'approach vector',approachVector
-			#EEgoalPosition=vectorops.add(goalPosition,vectorops.mul(approachVector,-probeLength))
+			approachVector=vectorops.unit(vectorops.mul(normals[i],-1.0)) #get unit vector in the direction '- normals'
+
 			## perform IK
 			pt1=goalPosition
-			pt2=vectorops.add(pt1,vectorops.mul(approachVector,1.0-probeLength))
+			pt2=vectorops.add(pt1,vectorops.mul(approachVector,1.0-probeLength)) # use 1m in normals direction.
 			localZUnitV=vectorops.cross([0,1,0],approachVector)
 			pt3=vectorops.add(pt1,localZUnitV)
 
@@ -157,9 +129,6 @@ def run_poking(config):
 			if res:
 				diff=vectorops.norm_L1(vectorops.sub(robotCurrentConfig,klampt_2_controller(robot.getConfig())))
 				EEZPos=link.getTransform()[1]
-				#print robotCurrentConfig
-				#print klampt_2_controller(robot.getConfig())
-				#print 'EE position',EEZPos
 				print 'difference', diff	
 				differences.append(diff)
 
@@ -175,15 +144,11 @@ def run_poking(config):
 				print "IK failture"
 				break
 
-
 			### now start colecting data..
-			#wrench = robotControlApi.getWrench()
-			#xForce = wrench[2]
 			travel = 0.0
-			#forceHistory=[xForce]
-			#displacementHistory=[travel]
 			stepVector = vectorops.mul(approachVector,moveStep)
-			while travel<0.0001:
+			
+			while travel<0.0001: #TODO: just try 0.1mm?
 				robotCurrentConfig=klampt_2_controller(robot.getConfig())
 				pt1=vectorops.add(pt1,stepVector)
 				pt2=vectorops.add(pt1,vectorops.mul(approachVector,1.0-probeLength))
@@ -194,10 +159,6 @@ def run_poking(config):
 				if res:
 					diff=vectorops.norm_L1(vectorops.sub(robotCurrentConfig,klampt_2_controller(robot.getConfig())))
 					EEZPos=link.getTransform()[1]
-					#print robotCurrentConfig
-					#print klampt_2_controller(robot.getConfig())
-					#print 'EE position',EEZPos
-					#print 'difference', diff	
 					if diff<IKErrorTolerence and EEZPos>EEZLimit:  #126 degrees
 						pass
 					else:
@@ -206,11 +167,7 @@ def run_poking(config):
 				else:
 					print "IK failture"
 					break
-				#time.sleep(dt)
-				#xForce = robotControlApi.getWrench()[2]
 				travel = travel + moveStep
-				#forceHistory.append(xForce)
-				#displacementHistory.append(travel)
 
 			### move the probe away
 			robotCurrentConfig=klampt_2_controller(robot.getConfig())
@@ -223,10 +180,6 @@ def run_poking(config):
 			if res:
 				diff=vectorops.norm_L1(vectorops.sub(robotCurrentConfig,klampt_2_controller(robot.getConfig())))
 				EEZPos=link.getTransform()[1]
-				#print robotCurrentConfig
-				#print klampt_2_controller(robot.getConfig())
-				#print 'EE position',EEZPos
-				#print 'difference', diff	
 				if diff<IKErrorTolerence and EEZPos>EEZLimit:  #126 degrees
 					pass
 				else:
@@ -240,24 +193,25 @@ def run_poking(config):
 			robot.setConfig(controller_2_klampt(robot,intermediateConfig))
 		
 		print('[*]Debug: Poking process done, with max difference:%f'%max(differences))
+		
+		vis.show()
+		while vis.shown():
+			time.sleep(1.0)
 
 	elif CONTROLLER == 'physical':
 		######################################## Ready to Take Measurements ################################################
 		#TODO: arrange the point execute order
 		exe_number = input('There are %d poking point, input executing number:'%len(points))
 		point_list == random.sample(range(len(points)),exe_number)
-		#K = np.arange(start = 132, stop = len(points)-1)
-		#print K
+
 		for i in point_list:	
 			robotCurrentConfig=robotControlApi.getConfig()
 			robot.setConfig(controller_2_klampt(robot,robotCurrentConfig))
 			#calculate start position
 			goalPosition=deepcopy(points[i])
 			approachVector=vectorops.unit(vectorops.mul(normals[i],-1.0))
-			#sapproachVector = [0,0,-1]
 			#### Make sure no contact, backup 0.01m
 			pt1=vectorops.add(goalPosition,vectorops.mul(approachVector,-0.01))
-			#pt1=goalPosition
 			pt2=vectorops.add(pt1,vectorops.mul(approachVector,1.0-probeLength))
 			localZUnitV=vectorops.cross([0,1,0],approachVector)
 			pt3=vectorops.add(pt1,localZUnitV)
@@ -267,10 +221,6 @@ def run_poking(config):
 			if res:
 				diff=vectorops.norm_L1(vectorops.sub(robotCurrentConfig,klampt_2_controller(robot.getConfig())))
 				EEZPos=link.getTransform()[1]
-				#print robotCurrentConfig
-				#print klampt_2_controller(robot.getConfig())
-				#print 'EE position',EEZPos
-				#print 'difference', diff	
 				if diff<IKErrorTolerence and EEZPos>EEZLimit:  #126 degrees
 					constantVServo(robotControlApi,longServoTime,klampt_2_controller(robot.getConfig()),dt)
 				else:
@@ -280,7 +230,6 @@ def run_poking(config):
 				print "IK failture"
 				break
 
-			#print klampt_2_controller(robot.getConfig())
 			time.sleep(0.2)
 
 			## Zero the sensor before straight line push
@@ -292,26 +241,25 @@ def run_poking(config):
 			counter = 0.0
 			totalF = [0,0,0]
 			startTime=time.time()
-			while (time.time()-startTime) < 1:
+			while (time.time()-startTime) < 1: # use 1s to cal the Force
 				totalF = vectorops.add(totalF,robotControlApi.getWrench()[0:3])
 				counter = counter + 1.0
 				time.sleep(dt)
+			forceBias = vectorops.mul(totalF,1.0/float(counter)) # when probe no touch the obj, F_avr = sum(F)/n
 
-			forceBias = vectorops.mul(totalF,1.0/float(counter))
-			#print forceBias
-
-
-			forceData=open(config.exp_path+'exp_'+str(config.exp_number)+'/force'+str(i)+'.txt','w')
-			#numberData=open('numberData'+str(i)+'.txt','w')
 			### now start collecting data..
+			# Force direction x, y inverse, refer to correct force.py
 			wrench = robotControlApi.getWrench()
 			Force = vectorops.sub(wrench[0:3],forceBias)
-			
+			Force_normal = math.fabs(vectorops.dot(Force,approachVector)) #|F||n|cos(theta) = F dot n, set it >= 0 
 			travel = -0.01
-			forceHistory=[Force]
-			displacementHistory=[travel]
+			
+			forceHistory = [-Force[0],-Force[1],Force[2]]			
+			force_normalHistory = [Force_normal]
+			displacementHistory = [travel]
 			stepVector = vectorops.mul(approachVector,moveStep)
-			while math.fabs(Force[2]) < forceLimit:
+
+			while Force_normal < forceLimit:
 				robotCurrentConfig=robotControlApi.getConfig()
 				robot.setConfig(controller_2_klampt(robot,robotCurrentConfig))
 				pt1=vectorops.add(pt1,stepVector)
@@ -332,18 +280,20 @@ def run_poking(config):
 					print "IK failture"
 					break
 				time.sleep(dt)
-				Force = vectorops.sub(robotControlApi.getWrench()[0:3],forceBias)
-				travel = travel + moveStep
-				forceHistory.append(Force)
-				displacementHistory.append(travel)
-			# TODO: x, y inverse, reference correct force.py
 
+				Force = vectorops.sub(robotControlApi.getWrench()[0:3],forceBias)
+				Force_normal = math.fabs(vectorops.dot(Force,approachVector))
+				travel = travel + moveStep
+
+				forceHistory.append([-Force[0],-Force[1],Force[2]])				
+				force_normalHistory.append(Force_normal)
+				displacementHistory.append(travel)
+			
 			#record all the data in 2 files, one N*2 containts all the force data collected at various locations, another
 			#file specifies the number of datapoints at each detected point
-			for (f,d) in zip(forceHistory,displacementHistory):
-				forceData.write(str(f[0])+' '+str(f[1])+' '+str(f[2])+' '+str(d)+'\n')
-
-			#numberData.write(str(len(forceHistory))+'\n')
+			forceData=open(config.exp_path+'exp_'+str(config.exp_number)+'/force_'+str(i)+'.txt','w')
+			for (f,fn,d) in zip(forceHistory,force_normalHistory,displacementHistory):
+				forceData.write(str(f[0])+' '+str(f[1])+' '+str(f[2])+' '+str(fn)+' '+str(d)+'\n')
 
 			### move the probe away
 
@@ -369,13 +319,46 @@ def run_poking(config):
 			forceData.close()
 			
 			#### move back to intermediate config
-			constantVServo(robotControlApi,shortServoTime,intermediateConfig,dt)
-			#numberData.close()		
+			constantVServo(robotControlApi,shortServoTime,intermediateConfig,dt)	
 			print'----------------------- pt '+str(i)+' completed -------------------------------'
 			
 		robotControlApi.stop()
+		
 
-	if CONTROLLER ==  "debugging":
-		vis.show()
-		while vis.shown():
-			time.sleep(1.0)
+def drop_code():
+	"""
+	drop code
+	"""
+	
+	"""
+
+	#emulator
+	#from RobotControllerEmulator import kill_controller_threads,UR5WithGripperController
+
+	'''
+	#CONTROLLER = 'simulation'
+	CONTROLLER = 'physical'
+	#CONTROLLER = 'debugging'
+
+	### Constants and Measurements
+	tableHeight = 0.87
+	probeLength = 0.09
+	forceLimit = 2.0
+	dt=0.004  #250Hz
+	moveStep=0.002*dt   #2mm /s
+	shortServoTime=1.5
+	longServoTime=3
+	IKErrorTolerence=4
+	maxDev=1.2
+	EEZLimit=0.956
+	'''
+	#import logging
+	#logging.basicConfig(level=logging.INFO)
+
+	# TODO: no use
+	if CONTROLLER == 'simulation':
+		controllerWorld = world.copy()
+	
+	"""
+
+
